bd681c2c8120a6e97465c10c4170ef07889aa392
nayon117 <hasibul.nayon1@gmail.com>
2024-01-15 15:40:55 +0600

    make the basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-indigo-700">
       <div className="bg-white rounded shadow pt-8 text-lg font-medium">
-        <h1 className="px-4">Counter App using React-Redux</h1>
+        <h1 className="px-4 text-center">Counter App using React-Redux</h1>
         <Counter/>
       </div>
     </div>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,13 @@
-
-
 const Counter = () => {
   return (
-    <div>
-      <h2 className="p-4 h-52 w-96 flex items-center justify-center">This is Counter</h2>
+      <div className="p-4 h-52 w-96 flex items-center justify-center ">
+          <div className="space-y-5">
+          <div className="text-2xl font-semibold text-center">0</div>
+          <div className="flex space-x-3">
+              <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow">Increment</button>
+              <button className="bg-red-400 text-white px-3 py-2 rounded shadow">Decrement</button>
+          </div>
+         </div>
     </div>
   );
 };
