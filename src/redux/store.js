771d14babbecec404cd1dfca766db741f93ae317
nayon117 <hasibul.nayon1@gmail.com>
2024-01-15 16:15:06 +0600

    create counter reducer function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
+import counterReducer from "./counter/counterReducer";
 // Todo: implement reducer function lately
-const store = createStore
+const store = createStore(counterReducer)
 
 export default store;
