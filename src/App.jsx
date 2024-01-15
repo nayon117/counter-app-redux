56ee78bf189c67d53020f39649adb1e553716cf9
nayon117 <hasibul.nayon1@gmail.com>
2024-01-15 15:30:42 +0600

    create counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
   return (
-    <>
-      <h1>Counter App</h1>
-    </>
+    <div className="flex h-screen items-center justify-center bg-indigo-700">
+      <div className="bg-white rounded shadow pt-8 text-lg font-medium">
+        <h1 className="px-4">Counter App using React-Redux</h1>
+        <Counter/>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+
+
+const Counter = () => {
+  return (
+    <div>
+      <h2 className="p-4 h-52 w-96 flex items-center justify-center">This is Counter</h2>
+    </div>
+  );
+};
+export default Counter;
